d9ea4628a8e94387dd270a9820934d62751feacf
Yourhearthh <1286386586@qq.com>
2021-02-14 14:33:32 +0800

    加入springSecurity权限框架的配置

--- src/main/java/com/louis/mango/controller/SysDictController.java
+++ src/main/java/com/louis/mango/controller/SysDictController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @Api(tags = {"字典管理接口"})
@@ -16,12 +17,14 @@ public class SysDictController {
     @Autowired
     SysDictService sysDictService;
 
+    @PreAuthorize("hasAuthority('sys:dict:view')")
     @ApiOperation(value = "分页查询字典数据", notes = "分页查询字典数据")
     @PostMapping(value = "/findPage")
     public HttpResult findPage(@RequestBody PageRequest pageRequest) {
         return HttpResult.ok(sysDictService.findPage(pageRequest));
     }
 
+    @PreAuthorize("hasAuthority('sys:dict:view')")
     @ApiOperation(value = "根据label查询字典数据", notes = "根据label查询字典数据")
     @ApiImplicitParam(name = "label", value = "标签名字", paramType = "query", dataType = "String")
     @GetMapping(value = "/findByLabel")
